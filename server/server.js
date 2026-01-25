7231acd6e1b326de5d2e81c5e963a65999d3aaec
shriom17 <129147531+shriom17@users.noreply.github.com>
2026-01-25 22:13:16 +0530

    db bug fixed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
-import connectDB from "./config/db.js";
+import connectDB from "./src/config/db.js";
 import { startCleanupScheduler } from "./src/services/cleanupService.js";
 
 const PORT = process.env.PORT || 5000;
