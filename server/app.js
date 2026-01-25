d5e232e750a32c21e649590fd1efb5d6d4cd97fc
shriom17 <129147531+shriom17@users.noreply.github.com>
2026-01-25 22:16:08 +0530

    bug fixed

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import feedbackRoute from './src/routes/feedBackRoute.js';
+import feedbackRoute from './src/routes/feedbackRoute.js';
 
 dotenv.config();
 
